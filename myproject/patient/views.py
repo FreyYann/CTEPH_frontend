0752361b21207ffb333423237e799d7290607e7a
NicksComingUp <remember.marsman@gmail.com>
2019-08-03 01:15:33 -0400

    adding a test route for sending a fetch request to python backend

--- myproject/patient/views.py
+++ myproject/patient/views.py
@@ -2,20 +2,11 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask import Flask, flash, request, redirect, url_for
 # from myproject import db
 import os
-import sys
-sys.path.append('/home/ItDev_Billy/itlize_demo/myproject')
-
-import SimpleITK as sitk
-from config import *
+# todo create folders for differents users
+from myproject.config import UPLOAD_FOLDER
+# from myproject.models import Owner
+# from myproject.owners.forms import AddForm
 from werkzeug.utils import secure_filename
-from patient import transformer
-from PIL import Image
-# import matplotlib as mpl
-# if os.environ.get('DISPLAY','') == '':
-#     print('no display found. Using non-interactive Agg backend')
-#     mpl.use('Agg')
-# import matplotlib.pyplot as plt
-import numpy as np
 
 ALLOWED_EXTENSIONS = set(['dcm'])
 patient_blueprint = Blueprint('patient',
@@ -27,21 +18,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def transormation(dir1,dir2):
-    a = transformer.Adjustor()
-    for f in os.listdir(dir1):
-        path=os.path.join(dir1,f)
-        if 'dcm' in f:
-            prefix = f.split('.')[0]
-            # plt.figure(figsize=(30,30))
-            arr = a.get_pixeldata(os.path.join(dir1,f), 'Lung3')[0]
-            # plt.imshow(np.array(arr),cmap='gray')
-
-            img = Image.fromarray(np.array(arr).astype('uint8'))
-            img.save(os.path.join(dir2,'%s.png' % prefix))
-
-            # plt.savefig(os.path.join(dir2,'%s1.png' % prefix))
-
 
 @patient_blueprint.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -52,9 +28,11 @@ def upload_file():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(UPLOAD_FOLDER, filename))
-        # turn dcm to png
-        transormation(UPLOAD_FOLDER,IMAGE_FOLDER)
-        dirlist = os.listdir(IMAGE_FOLDER)
+
+            # return redirect(url_for('main.upload',
+            #                 filename=filename))
+        # return 'Successfully uploaded to %s!' % UPLOAD_FOLDER
+        dirlist = os.listdir(UPLOAD_FOLDER)
         dummylist = ['' for x in dirlist]
         return render_template('inference.html', rangeX=range(len(dirlist)),
                                dirlist=dirlist, result=dummylist)
@@ -66,7 +44,7 @@ def upload_file():
 def inference():
     if request.method == 'POST':
         import os
-        dirlist = os.listdir(IMAGE_FOLDER)
+        dirlist = os.listdir(UPLOAD_FOLDER)
         # os.system("echo 'hello world'>> /home/ubuntu/1.txt")
         # os.system('source activate p36')
         if dirlist:
@@ -84,7 +62,9 @@ def inference():
     return render_template('result.html')
 
 
-
 @patient_blueprint.route('/about', methods=['GET', 'POST'])
-def about():
+def QA():
+    if request.method == 'POST':
+        print("TEST. HELLO FILE SENT HERE");
+        return "Sending back this"
     return render_template('about.html')
